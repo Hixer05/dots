e68e4e556945507eca77d1f9a93ffd21517f52aa
William Brusa <williambrusa@airmail.cc>
2024-09-20 23:42:19 +0200

    zshrc support in setup.sh

--- setup.sh
+++ setup.sh
@@ -18,11 +18,13 @@ if [ -L ./doom/default-config.el ]; then
     rm ./doom/default-config.el
 fi
 
-
 if [ "$1" == hostnames[0] ]; then
     ln -s ./doom/thinkpad-config.el ./doom/default-config.el
 elif [ "$1" == hostnames[1] ]; then
          ln -s ./doom/main-config.el ./doom/default-config.el
 fi
 
+if ! [ -e ../.zshrc ] || [ -L ../.zshrc ]; then
+    ln .zshrc ../.zshrc
+fi
 echo "Done"
