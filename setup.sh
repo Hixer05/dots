fc66ba42c3a3e4c90a9b8b7f7d06b349405bdd39
William Brusa <williambrusa@airmail.cc>
2024-09-20 23:36:14 +0200

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env sh
+
+hostnames=("thinkpad" "main")
+
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
     echo "./setup.sh <hostname>"
+    exit
 fi
 
-if ["$1" -eq "thinkpad"]; then
+if ! [[ "${hostnames[@]}" =~ "$1" ]]; then
+    echo "Hostname not found"
+    exit
+fi
+
+if [ -L ./doom/default-config.el ]; then
+    rm ./doom/default-config.el
+fi
+
+
+if [ "$1" == hostnames[0] ]; then
     ln -s ./doom/thinkpad-config.el ./doom/default-config.el
-elif ["$1" -eq "main"]; then
+elif [ "$1" == hostnames[1] ]; then
          ln -s ./doom/main-config.el ./doom/default-config.el
 fi
+
+echo "Done"
