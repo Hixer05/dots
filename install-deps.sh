69dca6fb95d2440c99ee67b10ee48609b6881e68
William Brusa <williambrusa@airmail.cc>
2024-10-25 13:34:10 +0200

    zsh vi mode, deps install

--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH_CUSTOM/plugins/zsh-vi-mode
 
